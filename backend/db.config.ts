6203057beb2e1c9812289ad84b24f2172759776d
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-03-18 14:57:13 -0400

    updated db.config.ts

--- backend/db.config.ts
+++ backend/db.config.ts
@@ -1,34 +1,14 @@
 import dotenv from 'dotenv';
+import {Client} from 'pg';
 dotenv.config();
 
-// export default {
-//   HOST: process.env.POSTGRESQL_DB_HOST,
-//   USER: process.env.POSTGRESQL_DB_USER,
-//   PASSWORD: process.env.POSTGRESQL_DB_PASSWORD,
-//   DB: process.env.POSTGRESQL_DB,
-//   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false // added for self-signed certificates
-//     }
-//   }
-// }; 
-import { Pool } from 'pg';
-
-const db = new Pool({
+const client = new Client({
     host: process.env.POSTGRESQL_DB_HOST,
     user: process.env.POSTGRESQL_DB_USER,
-    port: process.env.POSTGRESQL_DB_PORT ? parseInt(process.env.POSTGRESQL_DB_PORT) : 5432,
     password: process.env.POSTGRESQL_DB_PASSWORD,
     database: process.env.POSTGRESQL_DB,
+    port: process.env.POSTGRESQL_DB_PORT ? parseInt(process.env.POSTGRESQL_DB_PORT) : 5432,
+    ssl: true
 });
-const client = db.connect();
-db.connect((err) => {
-    if (err) {
-        console.error('connection error', err.stack);
-    } else {
-        console.log('connected');
-    }
-});
-export default db;
+
+export default client;
