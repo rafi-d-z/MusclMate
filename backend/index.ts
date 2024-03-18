0a7ce8ba2c143fc9b01f347acd06416a1abbf018
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-03-18 15:45:53 -0400

    edited index

--- backend/index.ts
+++ backend/index.ts
@@ -20,7 +20,6 @@ async function activate_db() {
 }
 activate_db();
 
-// process.exit();
 // doc: https://expressjs.com/en/4x/api.html
 const app: express.Application = express();
 const port: number = 3000;
@@ -29,13 +28,18 @@ app.get('/', (_req, _res) => {
 	_res.send("TypeScript With Express");
 });
 
-app.get('/common_excerises', (_req, _res) => {
+app.get('/get_exercise', async (_req, _res) => {
+  const res = await client.query('SELECT $1::text as message', ['Hello world!']);
   _res.send("excercises");
-})
+});
 
-app.get('/user_data', (_req, _res) => {
+app.post('/create_exercise', (_req, _res) => {
   _res.send("user's data");
-})
+});
+
+app.post('/edit_exercise', (_req, _res) => {
+  _res.send("user's data");
+});
 
 // Server setup
 app.listen(port, () => {
