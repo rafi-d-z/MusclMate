84b9e692f942c8c2e9a26ba16292ce7a26f777a8
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-03-18 15:11:05 -0400

    index ts called config for db correctly

--- backend/index.ts
+++ backend/index.ts
@@ -1,32 +1,28 @@
 import express from 'express';
-import db from './db.config';
+import client from './db.config';
 import dotenv from 'dotenv';
 dotenv.config();
 
-db.connect((err) => {
-    if (err) {
-        console.error('connection error', err.stack);
-    } else {
-        console.log('connected');
+// postgres aws db
+async function activate_db() {
+    console.log("Running ...")
+    try {
+        await client.connect();
+
+        const res = await client.query('SELECT $1::text as message', ['Hello world!']);
+        console.log(res.rows[0].message); // Hello world!
+    } catch (err: any) {
+        console.error('Database connection error', err.stack);
+    } finally {
+        await client.end();
     }
-}); 
+}
+activate_db();
 
 // process.exit();
 // doc: https://expressjs.com/en/4x/api.html
 const app: express.Application = express();
 const port: number = 3000;
-
-// postgres aws db
-// db.sequelize.authenticate().then(() => {
-//       console.log("Connected to the database!");
-//     })
-//     .catch((err: any) => {
-//       console.log("Cannot connect to the database!", err);
-//       process.exit();
-//     });
-
-// // sync
-// db.sequelize.sync()
 
 app.get('/', (_req, _res) => {
 	_res.send("TypeScript With Express");
