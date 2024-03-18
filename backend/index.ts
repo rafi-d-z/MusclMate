f98c301e7d804d35ec308d93f405a3dc332eb78e
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-03-18 16:09:42 -0400

    template input gating code

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import client from './db.config';
 import dotenv from 'dotenv';
+import { Client } from 'pg';
 dotenv.config();
 
 // postgres aws db
@@ -8,7 +9,7 @@ async function activate_db() {
     console.log("Connecting to Database ...");
     try {
         await client.connect();
-
+        return client;
         const res = await client.query('SELECT $1::text as message', ['Hello world!']);
         console.log(res.rows[0].message); // Hello world!
     } catch (err: any) {
@@ -18,7 +19,7 @@ async function activate_db() {
     }
     console.log("Database connected");
 }
-activate_db();
+let client_instance = activate_db();
 
 // doc: https://expressjs.com/en/4x/api.html
 const app: express.Application = express();
@@ -29,8 +30,20 @@ app.get('/', (_req, _res) => {
 });
 
 app.get('/get_exercise', async (_req, _res) => {
-  const res = await client.query('SELECT $1::text as message', ['Hello world!']);
-  _res.send("excercises");
+  const query = _req.query;
+  
+  if (
+    typeof query.target === 'string' &&
+    query.target.trim() !== '' &&
+    Array.isArray(query.keywords) &&
+    query.keywords.length > 0
+  ) {
+    console.log("Proper Query Received");
+  } else {
+    _res.status(404).send('Invalid input');
+  }
+  // client_instance;
+  // _res.send("excercises");
 });
 
 app.post('/create_exercise', (_req, _res) => {
