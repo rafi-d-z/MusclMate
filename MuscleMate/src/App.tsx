c2c97a59478aa25b3ead60112af9e1ba087927e6
jkim3273 <jkim4264@gmail.com>
2024-03-30 21:05:40 -0400

    added logo and header components

--- MuscleMate/src/App.tsx
+++ MuscleMate/src/App.tsx
@@ -8,139 +8,206 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
   Tabs,
   TabsContent,
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs"
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
 import './App.css'
+import muscleLogo from './assets/MuscleLogo.png'
 
 function MainMenu() {
 
   return (
-    <Tabs defaultValue="trending" className="w-[1200px]">
-      <TabsList className="grid w-full grid-cols-4">
-        <TabsTrigger value="trending">Trending</TabsTrigger>
-        <TabsTrigger value="abs">Abs</TabsTrigger>
-        <TabsTrigger value="swimming">Swimming</TabsTrigger>
-        <TabsTrigger value="running">Running</TabsTrigger>
-      </TabsList>
-      <TabsContent value="trending" className = "grid grid-cols-5">
-        <Card className="w-[200px]">
-          <CardHeader>
-            <CardTitle>Trending</CardTitle>
-            <CardDescription>Trends For You</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+    <>
+      <div className="flex items-center justify-between p-6 lg:px-8">
+        <img src={muscleLogo} width={200} height={200}/>
+        <div className="mt-5 flex lg:ml-4 gap-20">
+          <Input placeholder="Search" className="w-[200px] "/>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" className ="text-sm font-semibold leading-6 text-gray-900">Dashboard</Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Edit profile</SheetTitle>
+                <SheetDescription>
+                  Make changes to your profile here. Click save when you're done.
+                </SheetDescription>
+              </SheetHeader>
+              <div className="grid gap-4 py-4">
+                This is where we will have the buttons.
+              </div>
+              <SheetFooter>
+                <SheetClose asChild>
+                  <Button type="submit">Save changes</Button>
+                </SheetClose>
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
 
-        <Card className="w-[200px]">
-          <CardHeader>
-            <CardTitle>Trending</CardTitle>
-            <CardDescription>Trends For You</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+      <div>
+        <Tabs defaultValue="trending" className="w-[1200px]">
+          <TabsList className="grid w-full grid-cols-4">
+            <TabsTrigger value="trending">Trending</TabsTrigger>
+            <TabsTrigger value="abs">Abs</TabsTrigger>
+            <TabsTrigger value="swimming">Swimming</TabsTrigger>
+            <TabsTrigger value="running">Running</TabsTrigger>
+          </TabsList>
+          <TabsContent value="trending" className = "grid grid-cols-5">
+            <Card className="w-[200px]">
+              <CardHeader>
+                <CardTitle>Exercise 1</CardTitle>
+                <CardDescription>Trends For You</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
 
-        <Card className="w-[200px]">
-          <CardHeader>
-            <CardTitle>Trending</CardTitle>
-            <CardDescription>Trends For You</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+            <Card className="w-[200px]">
+              <CardHeader>
+                <CardTitle>Exercise 2</CardTitle>
+                <CardDescription>Trends For You</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
 
-      </TabsContent>
-      <TabsContent value="abs">
-        <Card>
-          <CardHeader>
-            <CardTitle>Password</CardTitle>
-            <CardDescription>
-              Change your password here. After saving, you'll be logged out.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="current">Current password</Label>
-              <Input id="current" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">New password</Label>
-              <Input id="new" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save password</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="swimming">
-        <Card>
-          <CardHeader>
-            <CardTitle>Password</CardTitle>
-            <CardDescription>
-              Change your password here. After saving, you'll be logged out.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="current">Current password</Label>
-              <Input id="current" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">New password</Label>
-              <Input id="new" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save password</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="running">
-      <Card className="w-[350px]">
-          <CardHeader>
-            <CardTitle>Trending</CardTitle>
-            <CardDescription>Trends For You</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-        <Card className="w-[350px]">
-          <CardHeader>
-            <CardTitle>Trending</CardTitle>
-            <CardDescription>Trends For You</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-    </Tabs>
+            <Card className="w-[200px]">
+              <CardHeader>
+                <CardTitle>Exercise 3</CardTitle>
+                <CardDescription>Trends For You</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+
+          </TabsContent>
+          <TabsContent value="abs" className = "grid grid-cols-5">
+            <Card className="w-[200px]">
+              <CardHeader>
+                <CardTitle>Trending</CardTitle>
+                <CardDescription>Trends For You</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+
+            <Card className="w-[200px]">
+              <CardHeader>
+                <CardTitle>Trending</CardTitle>
+                <CardDescription>Trends For You</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+
+            <Card className="w-[200px]">
+              <CardHeader>
+                <CardTitle>Trending</CardTitle>
+                <CardDescription>Trends For You</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+
+          </TabsContent>
+          <TabsContent value="swimming" className = "grid grid-cols-5">
+            <Card className="w-[200px]">
+              <CardHeader>
+                <CardTitle>Trending</CardTitle>
+                <CardDescription>Trends For You</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+
+            <Card className="w-[200px]">
+              <CardHeader>
+                <CardTitle>Trending</CardTitle>
+                <CardDescription>Trends For You</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+
+          </TabsContent>
+          <TabsContent value="running" className = "grid grid-cols-5">
+            <Card className="w-[200px]">
+                <CardHeader>
+                  <CardTitle>Trending</CardTitle>
+                  <CardDescription>Trends For You</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                  <p>Card Footer</p>
+                </CardFooter>
+              </Card>
+              <Card className="w-[200px]">
+                <CardHeader>
+                  <CardTitle>Trending</CardTitle>
+                  <CardDescription>Trends For You</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                  <p>Card Footer</p>
+                </CardFooter>
+              </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   )
 }
 
