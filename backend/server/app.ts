9d8256e19362e2c041845ab0b29ddc9c6fad366b
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-04-10 13:38:16 -0400

    commenting out get exercise endpoint because it needs reworking

--- backend/server/app.ts
+++ backend/server/app.ts
@@ -22,53 +22,54 @@ async function create_app(): Promise<express.Application>{
       _res.status(200).send("TypeScript With Express");
     });
 
-    app.get('/get_exercise', async (_req, _res) => {
-    // checking to see if input is valid or nah
-    const query = _req.query;
-    const target_is_string: Boolean = isString(query.target);
-    const keywords: any = toArray(query.keywords);
-    const keywords_is_array = Array.isArray(keywords);
+    // app.get('/get_exercise', async (_req, _res) => {
+    //   // checking to see if input is valid or nah
+    //   const query: any = _req.query;
+    //   const target_is_string: Boolean = isString(query.target);
+    //   const keywords: string[] | null = toArray(query.keywords);
+    //   const keywords_is_array: Boolean = Array.isArray(keywords);
 
-    if (!target_is_string && keywords_is_array){
-        _res.status(404).send("Invalid Input\n`keywords` not an array!");
-        return null;
-    } else if (target_is_string && !keywords_is_array){
-        _res.status(404).send("Invalid Input\n`target` not a string!");
-        return null;
-    } else if (!target_is_string && !keywords_is_array) {
-        _res.status(404).send('Invalid input\nExpected `target` as an alphanumeric string and `keywords` as array!');
-        return null;
-    }
-    try{ 
-        let query_str: string = `SELECT * FROM public.exercises WHERE exercise_target = $1`;
-        let params = [query.target];
+    //   if (!target_is_string && keywords_is_array){
+    //       _res.status(404).send("Invalid Input\n`keywords` not an array!");
+    //       return null;
+    //   } else if (target_is_string && !keywords_is_array){
+    //       _res.status(404).send("Invalid Input\n`target` not a string!");
+    //       return null;
+    //   } else if (!target_is_string && !keywords_is_array) {
+    //       _res.status(404).send('Invalid input\nExpected `target` as an alphanumeric string and `keywords` as array!');
+    //       return null;
+    //   }
+    //   try{ 
+    //       let query_str: string = `SELECT * FROM public.exercises WHERE exercise_target = $1`;
+    //       let params:string[] = [];
+    //       params.push(query.target);
 
-        if(keywords.length > 0) {
-        let keywords_str: string = 'AND (';
-        for (let i = 2; i < keywords.length + 2; i++) {
-            keywords_str += `$${i} = ANY(arr_keywords)`;
-            if (i < keywords.length + 1) {
-            keywords_str += ' OR ';
-            }
-            params.push(keywords[i]);
-        }
+    //       if(keywords.length > 0) {
+    //       let keywords_str: string = 'AND (';
+    //       for (let i = 2; i < keywords.length + 2; i++) {
+    //           keywords_str += `$${i} = ANY(arr_keywords)`;
+    //           if (i < keywords.length + 1) {
+    //           keywords_str += ' OR ';
+    //           }
+    //           params.push(keywords[i]);
+    //       }
 
-        keywords_str += ')';
-        query_str += ' ' + keywords_str;
-        }
-        if (client_instance !== undefined){
-        const res = await client_instance.query(query_str, params);
-        _res.status(200).send(res.rows);
-        return;
-        } else{
-        _res.status(500).send("Database is currently unavaliable at the moment, please try again later.");
-        return;
-        }
-    } catch (err) {
-        _res.status(500).send("Failed to query database");
-        return null;
-    }
-    });
+    //       keywords_str += ')';
+    //       query_str += ' ' + keywords_str;
+    //       }
+    //       if (client_instance !== undefined){
+    //       const res = await client_instance.query(query_str, params);
+    //       _res.status(200).send(res.rows);
+    //       return;
+    //       } else{
+    //       _res.status(500).send("Database is currently unavaliable at the moment, please try again later.");
+    //       return;
+    //       }
+    //   } catch (err) {
+    //       _res.status(500).send("Failed to query database");
+    //       return null;
+    //   }
+    // });
 
     app.post('/create_exercise', (_req, _res) => {
         const e_name = _req.body.exercise_name;
