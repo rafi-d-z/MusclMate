6daa60ec55cfc0fdaa3af369875ba6727f93bc87
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-04-14 16:08:20 -0400

    made login state default - removed unused imports - increase readability

--- MuscleMate/src/Login.tsx
+++ MuscleMate/src/Login.tsx
@@ -1,15 +1,10 @@
 import {
     Card,
     CardContent,
-    CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useState, useEffect } from "react"
-import './App.css'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -18,10 +13,9 @@ import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
-  FormDescription,
-  FormField,
   FormItem,
   FormLabel,
+  FormField,
   FormMessage,
 } from "@/components/ui/form"
 
@@ -60,143 +54,141 @@ function Login() {
      
     function onSubmit(values: z.infer<typeof formSchema>) {
         console.log(values)
-    }
+    } 
 
     return (
-        <>
-        <div className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
-          <Tabs className="w-[700px]">
-            <TabsList className="grid w-full grid-cols-2">
-              <TabsTrigger value="login">Login</TabsTrigger>
-              <TabsTrigger value="signup">Signup</TabsTrigger>       
-            </TabsList>
+      <>
+      <div className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+        <Tabs defaultValue="login" className="w-[700px]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="login">Login</TabsTrigger>
+            <TabsTrigger value="signup">Signup</TabsTrigger>       
+          </TabsList>
 
-            <TabsContent value="login">
-              <Card className="w-[700px]">
-                <CardHeader>
-                  <CardTitle>Log In</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                            <FormField
-                            control={form.control}
-                            name="username"
-                            render={({ field }) => (
-                                <FormItem>
-                                <FormControl>
-                                    <Input type="username" placeholder="Username" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                                </FormItem>
-                            )}
-                            />
-                            <FormField
-                            control={form.control}
-                            name="password"
-                            render={({ field }) => (
-                                <FormItem>
-                                <FormControl>
-                                    <Input type="password" placeholder="Password" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                                </FormItem>
-                            )}
-                            />
-                            <FormField
-                            control={form.control}
-                            name="remember"
-                            render={({ field }) => (
-                              <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md p-4">
-                                <FormControl>
-                                  <Checkbox
-                                    checked={field.value}
-                                    onCheckedChange={field.onChange}
-                                  />
-                                </FormControl>
-                                <div className="space-y-1 leading-none">
-                                  <FormLabel>
-                                    Remember Me
-                                  </FormLabel>
-                                </div>
-                              </FormItem>
-                            )}
-                            />                             
-                            <Button type="submit">Login</Button>
-                        </form>
-                      </Form>
-                  </CardContent>
-                </Card>
-              </TabsContent>
+          {/* login form */}
+          <TabsContent value="login">
+            <Card className="w-[700px]">
+              <CardHeader>
+                <CardTitle>Log In</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                      control={form.control}
+                      name="username"
+                      render={({ field }) => (
+                        <FormItem>
+                        <FormControl>
+                            <Input type="username" placeholder="Username" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}/>
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                        <FormControl>
+                            <Input type="password" placeholder="Password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}/>
+                    <FormField
+                      control={form.control}
+                      name="remember"
+                      render={({ field }) => (
+                      <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md p-4">
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                          />
+                        </FormControl>
+                        <div className="space-y-1 leading-none">
+                          <FormLabel>
+                            Remember Me
+                          </FormLabel>
+                        </div>
+                      </FormItem>
+                    )}/>                             
+                    <Button type="submit">Login</Button>
+                  </form>
+                </Form>
+              </CardContent>
+            </Card>
+          </TabsContent>
 
-              <TabsContent value="signup">
-                <Card className="w-[700px]">
-                  <CardHeader>
-                    <CardTitle>Sign Up</CardTitle>
-                  </CardHeader>
-                  <CardContent>
-                    <Form {...form}>
-                      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                          <FormField
-                          control={form.control}
-                          name="username"
-                          render={({ field }) => (
-                              <FormItem>
-                              <FormControl>
-                                  <Input type="username" placeholder="Username" {...field} />
-                              </FormControl>
-                              <FormMessage />
-                              </FormItem>
-                          )}
-                          />
-                          <FormField
-                          control={form.control}
-                          name="email"
-                          render={({ field }) => (
-                              <FormItem>
-                              <FormControl>
-                                <Input type="email" placeholder="Email" {...field} />
-                              </FormControl>
-                              <FormMessage />
-                              </FormItem>
-                          )}
-                          />
-                          <FormField
-                          control={form.control}
-                          name="password"
-                          render={({ field }) => (
-                              <FormItem>
-                              <FormControl>
-                                <Input type="password" placeholder="Password" {...field} />
-                              </FormControl>
-                              <FormMessage />
-                              </FormItem>
-                          )}
-                          />
-                          <FormField
-                          control={form.control}
-                          name="confirmPwd"
-                          render={({ field }) => (
-                              <FormItem>
-                              <FormControl>
-                                <Input type="password" placeholder="Confirm Password" {...field} />
-                              </FormControl>
-                              <FormMessage />
-                              </FormItem>
-                          )}
-                          />
-                                                    
-                          <Button type="submit">Sign Up</Button>
-                        </form>
-                      </Form>
-                  </CardContent>
-                </Card>
-              </TabsContent>
+          {/* signup form */}
+          <TabsContent value="signup">
+            <Card className="w-[700px]">
+              <CardHeader>
+                <CardTitle>Sign Up</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                      control={form.control}
+                      name="username"
+                      render={({ field }) => (
+                        <FormItem>
+                        <FormControl>
+                            <Input type="username" placeholder="Username" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                        <FormControl>
+                          <Input type="email" placeholder="Email" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                        <FormControl>
+                          <Input type="password" placeholder="Password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="confirmPwd"
+                      render={({ field }) => (
+                        <FormItem>
+                        <FormControl>
+                          <Input type="password" placeholder="Confirm Password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    <Button type="submit">Sign Up</Button>
+                  </form>
+                </Form>
+              </CardContent>
+            </Card>
+          </TabsContent>
 
-            </Tabs>
-        </div>
-        </>
-    )
+          </Tabs>
+      </div>
+      </>
+  )
 }
 
 export default Login
