ce826c7699aff0d51d8c384efe9e97e499d6426d
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-04-14 21:33:35 -0400

    set up jest for front end testing

---
+++ MuscleMate/jest.config.cjs
@@ -0,0 +1,9 @@
+/* eslint-env node */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  verbose: true,
+  forceExit: true,
+  // clearMocks: true,
+}
