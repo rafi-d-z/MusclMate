4a3ca8070499a9c99cbb197c1af51daf302df30c
Fahad <FFaruqi@dot.nyc.gov>
2024-04-18 17:39:38 -0400

    added database connection test

---
+++ backend/tests/connectToDatabase.test.ts
@@ -0,0 +1,10 @@
+import activate_db from "../server/db"
+import {Client} from 'pg';
+
+
+describe('Connects to database', () => {
+    test("should connect to database", async () => {
+        const client = await activate_db();
+        expect(client).toBe(Client);
+    })
+});
