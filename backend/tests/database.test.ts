a0913beff33a62c9f3a7cb6bc734246cd026d04b
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-04-19 16:22:07 -0400

    (backend) commented out create and delete exercises tests since they're depricated for now

--- backend/tests/database.test.ts
+++ backend/tests/database.test.ts
@@ -23,23 +23,23 @@ describe('Database Actions', () => {
     });
   });
 
-  describe('Create Exercise', () => {
-    test("create via paramaterized query", async () => {(
-      await request(app).post('/create_exercise').send({
-        name: 'test',
-        target: 'test',
-        reps: 10,
-        sets: 3,
-        keywords: encodeURIComponent(JSON.stringify(['test', 'test'])),
-        weight: 160
-      }).expect(200)
-    )})
+  // describe('Create Exercise', () => {
+  //   test("create via paramaterized query", async () => {(
+  //     await request(app).post('/create_exercise').send({
+  //       name: 'test',
+  //       target: 'test',
+  //       reps: 10,
+  //       sets: 3,
+  //       keywords: encodeURIComponent(JSON.stringify(['test', 'test'])),
+  //       weight: 160
+  //     }).expect(200)
+  //   )})
 
-    test("delete via parameterized query", async () => {(
-      await request(app).post('/delete').send({
-        db_name: "exercises",
-        uid: '4fdaa2ae-8016-417c-9718-6bc474f0387c'
-      }).expect(200)
-    )})
-  })
+    // test("delete via parameterized query", async () => {(
+    //   await request(app).post('/delete').send({
+    //     db_name: "exercises",
+    //     uid: '4fdaa2ae-8016-417c-9718-6bc474f0387c'
+    //   }).expect(200)
+    // )})
+  // })
 });
