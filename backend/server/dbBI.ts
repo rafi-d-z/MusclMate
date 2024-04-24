22320005b87e6e9fb3bd68223e6d88d0cdde75b9
Fahad <FFaruqi@dot.nyc.gov>
2024-04-24 13:26:20 -0400

    added function to dynamically fetch any sort of data from exercises

--- backend/server/dbBI.ts
+++ backend/server/dbBI.ts
@@ -1,4 +1,4 @@
-import { Client } from "pg";
+import { Client, query } from "pg";
 
 export async function get_exercise_by_uid(client: Client, uid: string): Promise<Array<any> | undefined>{
     const sql: string = "SELECT * FROM public.exercises WHERE uid = $1;"; 
@@ -12,6 +12,58 @@ export async function get_exercise_by_uid(client: Client, uid: string): Promise<
         return undefined;
     }
 }
+
+export async function get_exercises(client: Client, exercise_name: string = '', exercise_target: string = '', n_reps: number = 0, n_sets: number = 0, arr_keywords: Array<string> = [], weight: number = 0): Promise<Object | undefined> {
+    let conditions: Array<string> = [];
+    let values: Array<any> = [];
+    let index = 1;
+
+    if (exercise_name) {
+        conditions.push(`exercise_name = $${index++}`);
+        values.push(exercise_name);
+    }
+    if (exercise_target) {
+        conditions.push(`exercise_target = $${index++}`);
+        values.push(exercise_target);
+    }
+    if (n_reps > 0) {
+        conditions.push(`n_reps = $${index++}`);
+        values.push(n_reps);
+    }
+    if (n_sets > 0) { 
+        conditions.push(`n_sets = $${index++}`);
+        values.push(n_sets);
+    }
+    if (arr_keywords.length > 0) {
+        conditions.push(`arr_keywords && $${index++}`);
+        values.push(arr_keywords);
+    }
+    if (weight > 0) { 
+        conditions.push(`weight = $${index++}`);
+        values.push(weight);
+    }
+
+    const sql_string = `SELECT * FROM public.exercises WHERE ${conditions.join(' OR ')}`;
+
+    const query = {
+        name: "get_exercises",
+        text: sql_string,
+        values: values
+    }
+
+    const results = client.query(query);
+
+    query.on('end', () => {
+        return results;
+    });
+
+    query.on('error', (err) => {
+        console.error(err.stack);
+        return undefined;
+    });
+    return undefined;
+}
+
 
 export async function create_exercise(client: Client, item_name: string, target: string, reps: Number, sets: Number, keywords: string[], weight: Number): Promise<string | null>{
     const sql: string = `INSERT INTO public.exercises (uid, exercise_name, exercise_target, n_reps, n_sets,` +
