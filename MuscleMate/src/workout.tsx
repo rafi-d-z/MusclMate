cb0e8976ded0093cde3deaabe714662b2dae729a
CANCANNEED23 <114161096+CANCANNEED23@users.noreply.github.com>
2024-04-24 14:14:52 -0400

    Fixed issues for npm run build
    installed some dependencies for component, deleted unused import on workout page.

--- MuscleMate/src/workout.tsx
+++ MuscleMate/src/workout.tsx
@@ -12,16 +12,6 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import {
-    Sheet,
-    SheetClose,
-    SheetContent,
-    SheetDescription,
-    SheetFooter,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-} from "@/components/ui/sheet"
-import {
     Dialog,
     DialogClose,
     DialogContent,
@@ -47,6 +37,7 @@ import {
 } from "@/components/ui/popover"
 import './App.css'
 import muscleLogo from './assets/MuscleLogo.png'
+import { MenuBar } from "./components/ui/menubar"
 
 
 
@@ -73,35 +64,15 @@ function Workout() {
 
 
     return (
-        <>
-            <div className="flex items-center justify-between p-6 lg:px-8">
-                <img src={muscleLogo} width={200} height={200} />
-                <h1>My Workouts</h1>
-                <div className="mt-5 flex lg:ml-4 gap-20">
-                    <Input placeholder="Search" className="w-[200px] " />
-                    <Sheet>
-                        <SheetTrigger asChild>
-                            <Button variant="outline" className="text-sm font-semibold leading-6 text-gray-900">Dashboard</Button>
-                        </SheetTrigger>
-                        <SheetContent>
-                            <SheetHeader>
-                                <SheetTitle>Edit profile</SheetTitle>
-                                <SheetDescription>
-                                    Make changes to your profile here. Click save when you're done.
-                                </SheetDescription>
-                            </SheetHeader>
-                            <div className="grid gap-4 py-4">
-                                This is where we will have the buttons.
-                            </div>
-                            <SheetFooter>
-                                <SheetClose asChild>
-                                    <Button type="submit">Save changes</Button>
-                                </SheetClose>
-                            </SheetFooter>
-                        </SheetContent>
-                    </Sheet>
-                </div>
-            </div>
+    <>
+            {/* top bar components */}
+      <div className="flex items-center justify-between p-8 lg:px-8">
+        <img src={muscleLogo} width={200} height={200}/>
+        <div className="mt-5 flex lg:ml-4 gap-20">
+          <Input placeholder="Search" className="w-[200px] "/>
+          <MenuBar />
+        </div>
+      </div>
             <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', maxWidth: '360px' }}>
                 <div>
                     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', maxWidth: '800px' }}>
