b1b2b3288dae0009c0e2f4ca7bc009a310e07853
Fahad <FFaruqi@dot.nyc.gov>
2024-04-24 18:10:14 -0400

    added initial edit_workout unit test

---
+++ backend/tests/edit_workoutUT.test.ts
@@ -0,0 +1,33 @@
+import activate_db from "../server/db"
+import { Client } from "pg";
+import { edit_workout } from "../server/dbBI";
+import { workout } from "../server/DAO/workout";
+
+describe("create_workout unit tests", () => {
+    let client: Client;
+
+    beforeAll(async () => {
+        try{
+            client = await activate_db();
+        } catch (err){
+            console.error(err);
+        }
+    });
+
+
+    test("should edit a workout", async () => {
+        const new_workout: workout = {
+            uid: "any",
+            workout_name: "unit_test",
+            exercise_arr: ["5442fc3c-bcb0-4ba0-87a3-a05e3186b298", "6d481883-a599-44d5-9c45-8e4f57e6d917"],
+            keywords: ["unit-test", "unit-test", "unit-test"],
+        }
+        const result: string | undefined = await edit_workout(client, new_workout);
+        
+        if(result === undefined){
+            throw new Error("result is undefined");
+        }
+
+        expect(typeof(result)).toStrictEqual("string");
+    })
+})
