8b859d81d5df2563095fe5d1113473e6e5775c07
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-04-24 18:11:46 -0400

    fixed edit_workout - passes unit test

--- backend/tests/edit_workoutUT.test.ts
+++ backend/tests/edit_workoutUT.test.ts
@@ -17,10 +17,10 @@ describe("create_workout unit tests", () => {
 
     test("should edit a workout", async () => {
         const new_workout: workout = {
-            uid: "any",
-            workout_name: "unit_test",
+            uid: "fbd91776-5202-4737-ab90-ac5077b67f8d",
+            workout_name: "unit_test-EDITED",
             exercise_arr: ["5442fc3c-bcb0-4ba0-87a3-a05e3186b298", "6d481883-a599-44d5-9c45-8e4f57e6d917"],
-            keywords: ["unit-test", "unit-test", "unit-test"],
+            keywords: ["unit-test", "unit-test", "unit-test", "edited-unit-test"],
         }
         const result: string | undefined = await edit_workout(client, new_workout);
         
