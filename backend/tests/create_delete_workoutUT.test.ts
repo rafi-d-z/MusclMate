6a077d3aefc3ff87c791e25f07e198418c063747
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-04-24 19:10:49 -0400

    paired delete test with create test to avoid spam

--- backend/tests/create_delete_workoutUT.test.ts
+++ backend/tests/create_delete_workoutUT.test.ts
@@ -1,7 +1,9 @@
 import activate_db from "../server/db"
 import { Client } from "pg";
-import { create_workout } from "../server/dbBI";
+import { create_workout, delete_workout } from "../server/dbBI";
 import { workout } from "../server/DAO/workout";
+
+let uid: string;
 
 describe("create_workout unit tests", () => {
     let client: Client;
@@ -29,5 +31,18 @@ describe("create_workout unit tests", () => {
         }
 
         expect(typeof(result)).toStrictEqual("string");
+        uid = result;
+    });
+
+    test("should delete workout", async () => {
+        const workout_to_delete: workout = {
+            uid: uid,
+            workout_name: "unit_test",
+            exercise_arr: ["5442fc3c-bcb0-4ba0-87a3-a05e3186b298", "6d481883-a599-44d5-9c45-8e4f57e6d917"],
+            keywords: ["unit-test", "unit-test", "unit-test"],
+        }
+        const result: boolean | undefined = await delete_workout(client, workout_to_delete);
+
+        expect(result).toStrictEqual(true);
     })
 })
