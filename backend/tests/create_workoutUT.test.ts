f91aae55509458acb2816191b61daf34d94339be
Fahad <FFaruqi@dot.nyc.gov>
2024-04-24 17:39:45 -0400

    create_workout test

--- backend/tests/create_workoutUT.test.ts
+++ backend/tests/create_workoutUT.test.ts
@@ -24,20 +24,12 @@ describe("get_exercises unit tests", () => {
             exercise_arr: ["5442fc3c-bcb0-4ba0-87a3-a05e3186b298", "6d481883-a599-44d5-9c45-8e4f57e6d917"],
             keywords: ["unit-test", "unit-test", "unit-test"],
         }
-        const result: Array<object> | undefined = await get_exercises(client, exercise_name);
+        const result: Array<object> | undefined = await create_workout(client, new_workout);
 
         if(result === undefined){
             return;
         }
 
-        expect(result[0]).toStrictEqual({
-            uid: '22d65463-8176-4b0d-ba70-40ab5124cbda',
-            exercise_name: 'calf raises',
-            exercise_target: 'calves',
-            n_reps: 4,
-            n_sets: 15,
-            arr_keywords: [ 'calves', 'lower legs', 'hypertrophy' ],
-            weight: 160
-        })
+        expect(typeof(result)).toStrictEqual("string");
     })
 })
