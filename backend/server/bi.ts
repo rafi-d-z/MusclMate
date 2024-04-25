0de617995d288c3b16c97fa1eaf40fb46dcf0d20
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-04-24 23:07:32 -0400

    modified isString to check if a string is just symbols

--- backend/server/bi.ts
+++ backend/server/bi.ts
@@ -6,9 +6,11 @@ import { workout } from './DAO/workout';
  * @returns {Boolean} is_string
  */
 export function isString(input: any): Boolean{
+    const regex = /^[^\w\s]+$/;
     const is_string: Boolean = (
-        typeof input === 'string' && 
-        isNaN(Number(input))
+        typeof(input) === 'string' && 
+        isNaN(Number(input)) &&
+        regex.test(input) === false
     );
     return is_string;
 };
@@ -51,17 +53,34 @@ export function getWorkoutQueries(query: any): workout {
         exercise_arr: [],
         keywords: []
     };
+
     if(query.uid !== undefined){
+        if(!isString(query.uid)){
+            throw new Error("The 'uid' property of the query object is not a string")
+        }
         workout_query.uid = query.uid;
     } 
     if(query.workout_name !== undefined){
+        if(!isString(query.workout_name)){
+            throw new Error("The 'workout_name' property of the query object is not a string")
+        }
         workout_query.workout_name = query.workout_name;
     }
     if(query.exercise_arr !== undefined){
+        if(!isArray(query.exercise_arr)){
+            throw new Error("The 'exercise_arr' property of the query object is not an array")
+        }
         workout_query.exercise_arr = query.exercise_arr;
     }
     if(query.keywords !== undefined){
+        if(!isArray(query.keywords)){
+            throw new Error("The 'keywords' property of the query object is not an array")
+        }
         workout_query.keywords = query.keywords;
     }
+    if(!query.uid && !query.workout_name && !query.exercise_arr && !query.keywords){
+        throw new Error("No workout object found in query")
+    }
+
     return workout_query;
 }
