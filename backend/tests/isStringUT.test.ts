38d19cdd2aca0e414a1a445cfa3cd9e6ac0a7700
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-04-24 23:07:42 -0400

    added unit test for isString function

---
+++ backend/tests/isStringUT.test.ts
@@ -0,0 +1,38 @@
+import activate_db from "../server/db"
+import { Client } from "pg";
+import { isString } from "../server/bi";
+
+describe("isString unit tests", () => {
+    describe("should return true on correct input", () => {
+        test("standard string", () => {
+            const input: string = "hello";
+            const result: Boolean = isString(input);
+            expect(result).toStrictEqual(true);
+        });
+
+        test("mixed letter number string", () => {
+            const input: string = "Hello123";
+            const result: Boolean = isString(input);
+            expect(result).toStrictEqual(true);
+        });
+
+        test("mixed letter symbol string", () => {
+            const input: string = "Hello!!!";
+            const result: Boolean = isString(input);
+            expect(result).toStrictEqual(true);
+        });
+    });
+
+    describe("should return false on incorrect input", () => {
+        test("standard number", () => {
+            const input: number = 123;
+            const result: Boolean = isString(input);
+            expect(result).toStrictEqual(false);
+        });
+        test("symbols", () => {
+            const input: string = "!@#$";
+            const result: Boolean = isString(input);
+            expect(result).toStrictEqual(false);
+        })
+    })
+})
