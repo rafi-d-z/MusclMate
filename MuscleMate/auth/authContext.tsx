b15ee66d38bfd524e6a1dc9957933120e79eaed5
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-02 17:17:20 -0400

    added authentication to login page, abstracted login and signup components

---
+++ MuscleMate/auth/authContext.tsx
@@ -0,0 +1,45 @@
+import React, { useContext, useState, useEffect, createContext } from "react";
+import config from "./firebase.config";
+import { onAuthStateChanged } from "firebase/auth";
+
+const AuthContext = createContext(null);
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // logs user out by default
+    const unsubscribe = onAuthStateChanged(config.auth, initializeUser);
+    return unsubscribe;
+  }, []);
+
+  async function initializeUser(user) {
+    if (user) {
+      setCurrentUser({ ...user });
+      setUserLoggedIn(true);
+    } else {
+      setCurrentUser(null);
+      setUserLoggedIn(false);
+    }
+
+    setLoading(false);
+  }
+
+  const value = {
+    userLoggedIn,
+    currentUser,
+    setCurrentUser,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
--- MuscleMate/src/Routes.tsx
+++ MuscleMate/src/Routes.tsx
@@ -1,23 +1,21 @@
-import {
-    createBrowserRouter,
-  } from "react-router-dom";
-  import MainMenu from "./App";
-  import Workout from "./workout";
-  import Login from "./Login";
-  
-  const Routes = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainMenu />
-    },
-    {
-      path: "/Workout",
-      element: <Workout />
-    },
-      {
-      path: "/login",
-      element: <Login />
-    }
-  ]);
-  
-  export default Routes
+import { createBrowserRouter } from "react-router-dom";
+import MainMenu from "./App";
+import Workout from "./workout";
+import Login from "./Login";
+
+const Routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainMenu />,
+  },
+  {
+    path: "/workout",
+    element: <Workout />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+export default Routes;
