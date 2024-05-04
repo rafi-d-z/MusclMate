f40c0dbae18d4784cce982c2a14ce039b3f7cd96
fahad <fahadfaruqi1@gmail.com>
2024-05-04 16:17:12 -0400

    adding new unit tests

---
+++ backend/tests/toArrayUT.test.ts
@@ -0,0 +1,12 @@
+import { toArray } from '../server/bi';
+
+describe('toArray', () => {
+    it('should convert a string to an array', () => {
+        const result = toArray('["hello"]');
+        expect(result).toEqual(['hello']);
+    });
+
+    it('should throw', () => {
+        expect(() => toArray({ a: 1, b: 2, c: 3 })).toThrow();
+    });
+});
