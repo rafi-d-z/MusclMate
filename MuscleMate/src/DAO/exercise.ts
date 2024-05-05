a365b545ca78104d7c92da85e0ea260c99d4864f
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-05 10:01:11 -0400

    added DAO objects

---
+++ MuscleMate/src/DAO/exercise.ts
@@ -0,0 +1,10 @@
+export interface exercise {
+    uid: string,
+    exercise_name: string,
+    exercise_target: string,
+    image_url: string,
+    n_reps: number,
+    n_sets: number,
+    weight: number,
+    arr_keywords: Array<string>
+}
---
+++ MuscleMate/src/DAO/workout.ts
@@ -0,0 +1,6 @@
+export interface workout {
+    uid: string,
+    workout_name: string,
+    exercise_arr: Array<string>,
+    keywords: Array<string>
+}
