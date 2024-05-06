8c1b4cf6758029cdc41a0580209e7f0c677ec35f
CANCANNEED23 <114161096+CANCANNEED23@users.noreply.github.com>
2024-05-06 17:28:40 -0400

    changed application name
    from musclemate to flaskbb

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ zip -r "MuscleMate_deploy-v1.zip" ./*
 aws s3 cp "MuscleMate_deploy-v1.zip" s3://cancanneed23-musclemate
 
 aws elasticbeanstalk create-application-version\
- --application-name MuscleMate \
+ --application-name flaskbb \
  --source-bundle S3Bucket="cancanneed23-musclemate",S3Key="MuscleMate_deploy-v1.zip" \
  --version-label v1 \
  --description "for assignment 10" \
--- terraform/elastic_beanstalk.tf
+++ terraform/elastic_beanstalk.tf
@@ -1,5 +1,5 @@
 resource "aws_elastic_beanstalk_application" "application" {
-    name = "MuscleMate"  
+    name = "flaskbb"  
 }
 
 resource "aws_elastic_beanstalk_environment" "environment" {
