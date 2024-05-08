e30b8e78e9bac8fbd46d56229024fd13e6c97b16
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-08 17:24:48 -0400

    added error handling

--- backend/server/app.ts
+++ backend/server/app.ts
@@ -122,8 +122,8 @@ async function create_app(): Promise<Array<any>>{
 
     try{
       exerciseQuery = await getExerciseQueries(query);
-    }catch(err){
-      _res.status(400).send(err);
+    }catch(err: any){
+      _res.status(400).send(err.toString());
       return;
     }
 
