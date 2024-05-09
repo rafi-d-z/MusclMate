2ba23eff18ff64765171c797fe5c29a19403604d
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-09 12:18:08 -0400

    updated login page from main

--- MuscleMate/src/Login.tsx
+++ MuscleMate/src/Login.tsx
@@ -1,196 +1,264 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-
-import { Button } from "@/components/ui/button"
+  Form,
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormField,
+  FormMessage,
+} from "@/components/ui/form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
-Form,
-FormControl,
-FormItem,
-FormLabel,
-FormField,
-FormMessage,
-} from "@/components/ui/form"
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import config from "../auth/firebase.config";
+import { useNavigate } from "react-router-dom";
+import React from "react";
 
-import {
-Tabs,
-TabsContent,
-TabsList,
-TabsTrigger,
-} from "@/components/ui/tabs"
 
-import { Checkbox } from "@/components/ui/checkbox"
-
-const formSchema = z.object({
-  username: z.string().min(6, {
-    message: "Username must be at least 6 characters.",
-  }),
+const signUpSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
   password: z.string().min(6, {
     message: "Password must be at least 6 characters.",
   }),
   remember: z.boolean().default(false).optional(),
   confirmPwd: z.string(),
-  email: z.string().email({ message: "Invalid email address" })
-})
+});
 
-function Auth() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+const signInFormSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+  remember: z.boolean().default(false).optional(),
+});
+
+
+function SignIn(){
+  const nav = useNavigate();
+  const goHome = () => nav("/");
+
+  const form = useForm<z.infer<typeof signInFormSchema>>({
+    resolver: zodResolver(signInFormSchema),
     defaultValues: {
-      username: "",
+      email: "",
+      password: "",
+      remember: true,
+    },
+  });
+
+  function signInHandler(values: z.infer<typeof signInFormSchema>) {
+    const auth = getAuth(config.app);
+
+    signInWithEmailAndPassword(auth, values.email, values.password)
+      .then(() => {
+        goHome();
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+      });
+  }
+
+  return(
+    <>
+    <TabsContent value="login">
+    <Card className="w-[700px]">
+      <CardHeader>
+        <CardTitle>Log In</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(signInHandler)}
+            className="space-y-8"
+          >
+          {/* <form onSubmit={(e)=>{
+            e.preventDefault();
+            form.handleSubmit(signInHandler)(e);
+            }}
+            className="space-y-8"
+          > */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="remember"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md p-4">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormLabel>Remember Me</FormLabel>
+                  </div>
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Login</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  </TabsContent>
+  </>
+  )
+}
+
+function SignUp() {
+  const nav = useNavigate();
+  const goHome = () => nav("/");
+
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
       password: "",
       remember: true,
       confirmPwd: "",
       email: "",
     },
-  })
-   
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
-  } 
+  });
+
+  function createUserHandler(values: z.infer<typeof signUpSchema>) {
+    const auth = getAuth(config.app);
+    console.log(values);
+
+    createUserWithEmailAndPassword(auth, values.email, values.password)
+      .then(() => {
+        goHome();
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+      });
+  }
 
   return (
     <>
+      <TabsContent value="signup">
+        <Card className="w-[700px]">
+          <CardHeader>
+            <CardTitle>Sign Up</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(createUserHandler)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          type="email"
+                          placeholder="Email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="Password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="confirmPwd"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="Confirm Password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Sign Up</Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </TabsContent>
+    </>
+  );
+}
+
+function Auth(){
+  return(<>
     <div className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
       <Tabs defaultValue="login" className="w-[700px]">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="login">Login</TabsTrigger>
-          <TabsTrigger value="signup">Signup</TabsTrigger>       
+          <TabsTrigger value="signup">Signup</TabsTrigger>
         </TabsList>
-
-        {/* login form */}
-        <TabsContent value="login">
-          <Card className="w-[700px]">
-            <CardHeader>
-              <CardTitle>Log In</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                  <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                      <FormItem>
-                      <FormControl>
-                        <Input type="username" placeholder="Username" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                      <FormControl>
-                        <Input type="password" placeholder="Password" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="remember"
-                    render={({ field }) => (
-                    <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md p-4">
-                      <FormControl>
-                        <Checkbox
-                          checked={field.value}
-                          onCheckedChange={field.onChange}
-                        />
-                      </FormControl>
-                      <div className="space-y-1 leading-none">
-                        <FormLabel>
-                          Remember Me
-                        </FormLabel>
-                      </div>
-                    </FormItem>
-                  )}/>                             
-                  <Button type="submit">Login</Button>
-                </form>
-              </Form>
-            </CardContent>
-          </Card>
-        </TabsContent>
-
-        {/* signup form */}
-        <TabsContent value="signup">
-          <Card className="w-[700px]">
-            <CardHeader>
-              <CardTitle>Sign Up</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                  <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                      <FormItem>
-                      <FormControl>
-                        <Input type="username" placeholder="Username" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                      </FormItem>
-                  )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                      <FormControl>
-                        <Input type="email" placeholder="Email" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                      </FormItem>
-                  )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                      <FormControl>
-                        <Input type="password" placeholder="Password" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                      </FormItem>
-                  )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="confirmPwd"
-                    render={({ field }) => (
-                      <FormItem>
-                      <FormControl>
-                        <Input type="password" placeholder="Confirm Password" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                      </FormItem>
-                  )}
-                  />
-                  <Button type="submit">Sign Up</Button>
-                </form>
-              </Form>
-            </CardContent>
-          </Card>
-        </TabsContent>
-
-        </Tabs>
+        <SignIn />
+        <SignUp />
+      </Tabs>
     </div>
-    </>
-)
+  </>)
 }
 
 export default Auth;
