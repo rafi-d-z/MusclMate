de651670535a592244927ca210c1874bb7023c11
rafi-d-z <rafidizia@gmail.com>
2024-05-13 13:28:12 -0400

    fixed user-page

--- MuscleMate/src/User.tsx
+++ MuscleMate/src/User.tsx
@@ -20,7 +20,7 @@ import exercise from "@/DAO/exercise";
 function User(){
     const [userExercises, setUserExercises] = useState<Array<any> | undefined>(undefined);
     const [userWorkouts, setUserWorkouts] = useState<Array<any> | undefined>(undefined);
-    const [uid, setUID] = useState<string | undefined>(undefined);
+    
     
 
     useEffect(() => {
@@ -28,13 +28,13 @@ function User(){
         onAuthStateChanged(authState, user => {
             if (user) {
                 console.log(user.uid)
-                setUID(user.uid);
+                
     
                 axios({
                     method: 'get',
                     url: 'https://api-muscleman.com/get_user',
                     params: {
-                        uid: uid
+                        uid: user.uid
                     }
                 })
                 .then(function (response) {
@@ -45,43 +45,43 @@ function User(){
             }
         });
     }, []); 
-    console.log(userExercises)
-    console.log(userWorkouts)
+        console.log(userExercises)
+        console.log(userWorkouts)
 
-    // get user information via user api (includes exercise and workouts)
-    return (
-        <>
-            <TopBar />
-            {/* user information
-                user workouts
-                user exercises */}
-            <div key="exercises">
-                <h1>Your Exercises</h1>
-                {userExercises && userExercises.length > 0 && 
-                    <div className="flex flex-row space-x-4 overflow-x-auto">
-                        {userExercises.map((exercise: exercise) => {
-                            return (
-                                <div key={exercise.uid} className="w-52">
-                                    <Card>
-                                        <CardHeader>
-                                            <CardTitle>{exercise.exercise_name}</CardTitle>
-                                            <CardDescription>{exercise.exercise_target}</CardDescription>
-                                        </CardHeader>
-                                        <CardContent>
-                                            <img src={exercise.image_url}></img>
-                                        </CardContent>
-                                        <CardFooter>
-                                            Reps: {exercise.n_reps} / Sets: {exercise.n_sets} / Weight: {exercise.weight}
-                                        </CardFooter>
-                                    </Card>
-                                </div>
-                            )
-                        })}
-                    </div>
-                }
-            </div>
+        // get user information via user api (includes exercise and workouts)
+        return (
+            <>
+                <TopBar />
+                {/* user information
+                    user workouts
+                    user exercises */}
+                <div key="exercises">
+                    <h1>Your Exercises</h1>
+                    {userExercises && userExercises.length > 0 && 
+                        <div className="flex flex-row space-x-4 overflow-x-auto">
+                            {userExercises.map((exercise: exercise) => {
+                                return (
+                                    <div key={exercise.uid} className="w-52">
+                                        <Card>
+                                            <CardHeader>
+                                                <CardTitle>{exercise.exercise_name}</CardTitle>
+                                                <CardDescription>{exercise.exercise_target}</CardDescription>
+                                            </CardHeader>
+                                            <CardContent>
+                                                <img src={exercise.image_url}></img>
+                                            </CardContent>
+                                            <CardFooter>
+                                                Reps: {exercise.n_reps} / Sets: {exercise.n_sets} / Weight: {exercise.weight}
+                                            </CardFooter>
+                                        </Card>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    }
+                </div>
 
-            <div key="workouts">
+                <div key="workouts">
                     <h1>Your Workouts</h1>
                     {userWorkouts && userWorkouts.length > 0 &&
                         <div className="flex flex-wrap space-x-4">
@@ -102,8 +102,8 @@ function User(){
                         </div>
                     }
                 </div>
-        </>
-    )
-}
+            </>
+        )
+    }
 
 export default User;
